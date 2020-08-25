45db5e6187a52441d08fe2be4e283d56f184c596
Dani-jardim <danialvesdesigner@gmail.com>
2020-08-25 19:41:04 -0300

    recebendo valor do input e criando uma nova <li>

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const formAddTodo = document.querySelector('.form-add-todo')
 
+const ul = document.querySelector('.todos-container')
+
+formAddTodo.addEventListener('submit', event => {
+  event.preventDefault()
+    const inputValue = event.target.add.value.trim()
+    ul.innerHTML +=`
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span> ${inputValue}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>`;
+    console.log(inputValue)
+})
