2d4e3234376a2fdfade9edb2a518f70c5bdad245
Dani-jardim <danialvesdesigner@gmail.com>
2020-09-01 11:18:21 -0300

    adicionando search no input

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const inputSearchTodo = document.querySelector('.form-search input')
-
 const todosContainer = document.querySelector('.todos-container')
 
 
@@ -27,11 +26,18 @@ todosContainer.addEventListener('click', event => {
 })
 
 inputSearchTodo.addEventListener('input', event => {
-  const inputValue =event.target.value.trim()
+  const inputValue = event.target.value.trim().toLowerCase()
   Array.from(todosContainer.children)
-    .filter( todo => !todo.textContent.includes(inputValue))
+    .filter( todo => !todo.textContent.toLowerCase().includes(inputValue))
     .forEach(todo => {
+      todo.classList.remove('d-flex')
       todo.classList.add('hidden')
+    })
+  Array.from(todosContainer.children)
+    .filter( todo => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove('hidden')
+      todo.classList.add('d-flex')
     })
 })
   
